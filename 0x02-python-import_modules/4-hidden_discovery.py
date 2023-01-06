da37ee3aac3f1675a6e07ceecd7ea260133ed3a3
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-06 21:58:53 +0000

    Python import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import sys
     import hidden_4
-    names_list = dir(hidden_4)
-    for name in names_list:
-        if name[0] != '_':
-            print("{}".format(name))
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
