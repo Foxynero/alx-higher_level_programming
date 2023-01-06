e1bff9ecd0ca340c16503726f9ac96f64cae70fa
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-06 21:58:18 +0000

    Python import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    summation = 0
-    argument_list = sys.argv[1:]
-    for argument in argument_list:
-        summation += int(argument)
-    print("{}".format(summation))
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+    print("{:d}".format(res))
