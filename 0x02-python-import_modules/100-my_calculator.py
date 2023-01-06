e244a954707b5995d83f1338310053acc6140c1c
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-06 21:54:50 +0000

    Python import modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    args_list = sys.argv[1:]
-    if len(args_list) != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
     from calculator_1 import add, sub, mul, div
-    a = int(args_list[0])
-    b = int(args_list[2])
-    if args_list[1] == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif args_list[1] == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif args_list[1] == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif args_list[1] == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count is not 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] is "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] is "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] is "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
