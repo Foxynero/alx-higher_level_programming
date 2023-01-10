8f6a01f965698e54fb0539d99d1cdd4b74273e7e
Foxynero <adugyamfifoster@gmail.com>
2023-01-10 07:26:19 -0800

    Python data structures

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -23,5 +23,6 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 int palindrome_check(listint_t *tmp_head, int count);
+void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
