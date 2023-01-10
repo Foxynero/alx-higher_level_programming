d2a3afeedb16a5e9a1311ad28172394e9749716a
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-10 15:20:23 +0000

    Python data structures

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,6 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
+int palindrome_check(listint_t *tmp_head, int count);
 
 #endif /* LISTS_H */
