bb089009d3107ec3b8684c2c3c32b4a3f6dc7e9f
Foxynero <adugyamfifoster@gmail.com>
2023-01-25 07:52:57 -0800

    Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,6 +8,6 @@ def magic_calculation(a, b):
             else:
                 result += a ** b / i
         except:
-            result = b + a
+            result = a + b
             break
-        return (result)
+    return result
