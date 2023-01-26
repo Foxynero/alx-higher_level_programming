7531aafe5b49d7a7d6b375a79ca6c4b611628605
Foxynero <adugyamfifoster@gmail.com>
2023-01-26 04:53:22 -0800

    Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += a ** b / i
-        except:
+        except Exception:
             result = a + b
             break
     return result
