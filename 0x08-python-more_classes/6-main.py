398eb5d6c58a9221e057499c42e7629bdc9cc146
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-30 12:27:51 +0000

    Python classes - more

--- 0x08-python-more_classes/6-main.py
+++ 0x08-python-more_classes/6-main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('6-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
