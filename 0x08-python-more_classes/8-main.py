66d30bee41e8b6fb0269ac4a0295939f95e6ef2f
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-30 12:29:17 +0000

    Python classes - more

--- 0x08-python-more_classes/8-main.py
+++ 0x08-python-more_classes/8-main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
+Rectangle = __import__('8-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+my_rectangle_1 = Rectangle(8, 4)
+my_rectangle_2 = Rectangle(2, 3)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+else:
+    print("my_rectangle_2 is bigger than my_rectangle_1")
+
+
+my_rectangle_2.width = 10
+my_rectangle_2.height = 5
+if my_rectangle_1 is Rectangle.bigger_or_equal(my_rectangle_1, my_rectangle_2):
+    print("my_rectangle_1 is bigger or equal to my_rectangle_2")
+else:
+    print("my_rectangle_2 is bigger than my_rectangle_1")
