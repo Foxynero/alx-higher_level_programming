ee139233f537e414991430a89a9cf19e9d5fb060
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-31 11:13:51 +0000

    Python test driven development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This is the module 4-print_square.
+It contains one function: print_square
+"""
+
+
+def print_square(size):
+    """
+    prints a square with the character #
+    """
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+        return
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        print("#" * size)
