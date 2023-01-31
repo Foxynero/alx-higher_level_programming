e6f2b1d93cc252c33571b1e196d76101b80063b6
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-31 14:35:25 +0000

    Python test driven development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This is the module 101-lazy_matrix_mul.
+It contains one function: 101-lazy_matrix_mul
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    returns the product of m_a and m_b
+    """
+    return numpy.matmul(m_a, m_b)
