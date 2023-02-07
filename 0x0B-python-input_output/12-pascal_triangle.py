000cf2e232c25bb7f0bb67f369263936f6567e4f
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-02-07 21:00:41 +0000

    Python input/output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+"""pascal_triangle
+"""
+
 
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
-
-    Returns a list of lists of integers representing the triangle.
+    """ends of each list in the matrix are summed to display the next list
+    until the middle of the list. The number '1' is always at both ends.
     """
     if n <= 0:
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-            tmp.append(1)
-            triangles.append(tmp)
-            return triangles
+    res = []
+    for elem in range(n):
+        if elem == 0:
+            res.append([1])
+            continue
+        if elem == 1:
+            res.append([1, 1])
+            continue
+        row = []
+        # init row
+        for item in range(elem + 1):
+            row.append(item)
+        for item in range(1, elem):
+            row[0] = 1
+            row[elem] = 1
+            row[item] = res[elem - 1][item] + res[elem - 1][item - 1]
+        res.append(row)
+            
+    return res
