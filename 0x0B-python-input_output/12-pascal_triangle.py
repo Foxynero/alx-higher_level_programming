2c3eb860d6bb29d7fff3bb1bc0daaa36e1ce2eb3
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-02-07 21:31:15 +0000

    Python input/output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,23 +3,26 @@
 
 
 def pascal_triangle(n):
-    """returns list representing Pascal's triangle"""
-    pascal_triangle = []
-    previous = [1]
+    """ends of each list in the matrix are summed to display the next list
+    until the middle of the list. The number '1' is always at both ends.
+    """
     if n <= 0:
-        return pascal_triangle
-    for row_number in range(n):
-        rowlist = []
-        if row_number == 0:
-            rowlist = [1]
-        else:
-            for i in range(row_number + 1):
-                if i == 0:
-                    rowlist.append(0 + previous[i])
-                elif i == (row_number):
-                    rowlist.append(previous[i - 1] + 0)
-                else:
-                    rowlist.append(previous[i - 1] + previous[i])
-        previous = rowlist
-        pascal_triangle.append(rowlist)
-        return pascal_triangle
+        return []
+
+    res = []
+    for elem in range(n):
+        if elem == 0:
+            res.append([1])
+            continue
+        if elem == 1:
+            res.append([1, 1])
+            continue
+        row = []
+        # init row
+        for item in range(elem + 1):
+            row.append(item)
+        for item in range(1, elem):
+            row[0] = 1
+            row[elem] = 1
+            row[item] = res[elem - 1][item] + res[elem - 1][item - 1]
+        res.append(row)
