115c1181fc9060f1955557fc9dbe80ec484cef90
Foxynero <adugyamfifoster@gmail.com>
2023-02-10 14:14:28 -0800

    Python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""MyList
-"""
+""" module 1-my_list contains the class MyList """
 
 
 class MyList(list):
-    """Contains list
-    """
+    """ defines MyList class """
+
+    def __init__(self):
+        """ initializes new MyList object """
+        pass
 
     def print_sorted(self):
-        """Prints self in sorted format
-        """
-
+        """ prints sorted list """
         print(sorted(self))
