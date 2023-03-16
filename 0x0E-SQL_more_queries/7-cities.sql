e618bcadfd02e315d016bb2c11dc51b18bd4eea0
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-03-16 17:10:31 +0000

    SQL more queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,7 @@
 -- Creates a database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities' (
-	PRIMARY KEY('id'),
-	'id' INT  NOT NULL AUTO_INCREMENT,
-	'state_id' INT NOT NULL,
-	'name' VARCHAR(256) NOT NULL,
-	FOREIGN KEY('state_id')
-	REFERENCES 'hbtn_0d_usa'.'states'('id')
-	);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+state_id INT NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id),
+name VARCHAR(256) NOT NULL);
