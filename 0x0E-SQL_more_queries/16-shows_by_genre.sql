0c6af74524deb63477cb07eb31a1f3290333595d
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-03-16 17:17:32 +0000

    SQL more queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,10 @@
 -- Lists all shows and genres linked to the show from the
 -- database hbtn_0d_tvshows.
 -- Records are ordered by ascending show title and genre name.
-SELECT t.'title', g.'name'
-FROM 'tv_shows' AS t
-LEFT JOIN 'tv_show_genres' AS s
-ON t.'id' = s.'show_id'
-
-LEFT JOIN 'tv_genres' AS g
-ON s.'genre_id' = g.'id'
-ORDER BY t.'title', g.'name';
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
