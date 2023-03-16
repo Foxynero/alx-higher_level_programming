e90e087b639c998f96635820005c24195a477052
Foxynero <adugyamfifoster@gmail.com>
2023-03-16 16:19:08 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,20 +1,14 @@
 -- Lists all genres of the database hbtn_0d_tvshows
 -- not linked to the show Dexter.
 -- Records are sorted by ascending genre name.
-SELECT DISTINCT 'name'
-  FROM 'tv_genres' AS g
-       INNER JOIN 'tv_show_genres' AS s
-       ON g.'id' = s.'genre_id'
-
-       INNER JOIN 'tv_shows' AS t
-       ON s.'show_id' = t.'id'
-       WHERE g.'name' NOT IN
-             (SELECT 'name'
-                FROM 'tv_genres' AS g
-	             INNER JOIN 'tv_show_genres' AS s
-		     ON g.'id' = s.'genre_id'
-
-		     INNER JOIN 'tv_shows' AS t
-		     ON s.'show_id' = t.'id'
-		     WHERE t.'title' = "Dexter")
- ORDER BY g.'name';
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN
+	(SELECT a.name AS name
+	FROM tv_genres AS a
+	JOIN tv_show_genres AS b
+	ON b.genre_id = a.id
+	JOIN tv_shows AS c
+	ON b.show_id = c.id
+	WHERE c.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,19 +1,13 @@
 -- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
 -- Records are ordered by ascending show title.
-SELECT DISTINCT 'title'
-  FROM 'tv_shows' AS t
-       LEFT JOIN 'tv_show_genres' AS s
-       ON s.'show_id' = t.'id'
-
-       LEFT JOIN 'tv_genres' AS g
-       ON g.'id' = s.'genre_id'
-       WHERE t.'title' NOT IN
-             (SELECT 'title'
-                FROM 'tv_shows' AS t
-	             INNER JOIN 'tv_show_genres' AS s
-		     ON s.'show_id' = t.'id'
-
-		     INNER JOIN 'tv_genres' AS g
-		     ON g.'id' = s.'genre_id'
-		     WHERE g.'name' = "Comedy")
- ORDER BY 'title';
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN
+	(SELECT a.title
+	FROM tv_shows AS a
+	JOIN tv_show_genres AS b
+	ON a.id = b.show_id
+	JOIN tv_genres AS c
+	ON b.genre_id = c.id
+	WHERE c.name = 'Comedy')
+ORDER BY tv_shows.title ASC;
