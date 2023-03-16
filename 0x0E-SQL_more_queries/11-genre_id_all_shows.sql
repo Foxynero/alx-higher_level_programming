18b919893b2e3edba88eabd9b48386f0dd8104de
Foxynero <adugyamfifoster@gmail.com>
2023-03-16 10:52:23 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,9 +1,6 @@
---  lists all shows contained in the database hbtn_0d_tvshows.
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- Displays NULL for shows without genres.
-USE hbtn_0d_tvshows;
+-- MySQL script to select shows
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
+FROM tv_show_genres
+RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-USE hbtn_0d_tvshows;
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.show_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
+FROM tv_show_genres
+RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title ASC;
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
