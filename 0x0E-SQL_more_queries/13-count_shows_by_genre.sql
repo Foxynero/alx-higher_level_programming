16974106ce562dcbc1f4e455aea8009f1502dcd8
Foxynero <adugyamfifoster@gmail.com>
2023-03-16 11:04:14 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- shows linked to each.
 -- Does not display genres without linked shows.
 -- Records are ordered by descending number of shows linked.
-SELECT name AS 'genre', COUNT(*) AS 'number_shows'
+SELECT name AS 'genre', COUNT(*) AS 'number_of_shows'
 FROM tv_genres, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+ORDER BY number_of_shows DESC;
