e2b9c4291220d8913f4ea8d5d72b852db9188cd7
Foxynero <adugyamfifoster@gmail.com>
2023-03-16 10:34:53 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,5 @@
--- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
--- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
-USE hbtn_0d_tvshows;
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-LEFT JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+FROM tv_shows, tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
