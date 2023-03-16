794b0171ca3d75f87b441a9da9c8915423c2c217
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-03-16 17:09:54 +0000

    SQL more queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,4 @@
 -- Creates the table hbtn_0d_usa with table states.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
-	PRIMARY KEY('id'),
-	'id' INT  NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR(256) NOT NULL
-	);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
