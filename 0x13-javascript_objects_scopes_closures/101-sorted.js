2e81d253bb3ec8bf7d3c8a8098fbf6b4c56d1346
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-04-17 15:13:49 +0000

    Javascript objects scopes closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-
-const listk = Object.keys(dict);
-const newdict = {};
-for (let con = 0; con < listk.length; con++) {
-	const nkey = dict[listk[con]];
-	if (newdict[nkey]) {
-		newdict[nkey].push(listk[con]);
-		} else {
-			newdict[nkey] = [listk[con]];
-		}
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
 }
-console.log(newdict);
+console.log(newDict);
