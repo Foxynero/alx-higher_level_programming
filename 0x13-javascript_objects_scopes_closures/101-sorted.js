79fa1976fb9ee874604076511beef76da7010fb6
Foxynero <adugyamfifoster@gmail.com>
2023-04-17 08:06:26 -0700

    Javascript objects scopes closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
+
+const listk = Object.keys(dict);
+const newdict = {};
+for (let con = 0; con < listk.length; con++) {
+	const nkey = dict[listk[con]];
+	if (newdict[nkey]) {
+		newdict[nkey].push(listk[con]);
+		} else {
+			newdict[nkey] = [listk[con]];
+		}
 }
-console.log(newDict);
+console.log(newdict);
