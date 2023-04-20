30628bb4dac5ced1ba34bb82d7ef9ff88803f342
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-04-20 19:04:16 +0000

    Object relational mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
         Session = sessionmaker(bind=engine)
         session = Session()
         for instance in session.query(State).order_by(State.id):
-                print("{}: {}".format(instance.id, instance.name))
+            print("{}: {}".format(instance.id, instance.name))
         session.close()
