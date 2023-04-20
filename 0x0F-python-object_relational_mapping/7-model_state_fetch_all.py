bcc4cd12089e46d5b11bf7f67c0b723e05b0cb59
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-04-20 11:37:03 -0700

    Object relational mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""
-lists all State objects from the database in ascending order
-using SQLAlchemy and importing State and Base from model_state
-"""
-from sys import argv
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+# list all state objects from the database
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for instance in session.query(State).order_by(State.id):
-                print("{}: {}".format(instance.id, instance.name))
-        session.close()
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.engine.url import URL
+
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': argv[1],
+          'password': argv[2],
+          'database': argv[3]}
+
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
