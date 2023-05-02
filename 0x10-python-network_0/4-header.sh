caf69b762e8b87e1a00ff2b2c5d631a1ab40354f
Foxynero <adugyamfifoster@gmail.com>
2023-05-02 05:29:40 -0700

    Python network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends GET request to given URL, with key-value, and displays body of response
-curl -sH "X-HolbertonSchool-User-Id:98" "$1"
+curl -sX GET "$1" -H "X-School-User-Id:98"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends POST request to given URL for two variables & displays body of response
-curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
