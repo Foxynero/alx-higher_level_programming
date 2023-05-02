260cda1dde1370d6f2f8a2dec0c8722bc8d3fcd8
Foxynero <adugyamfifoster@gmail.com>
2023-05-02 05:40:08 -0700

    Python network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends POST request with JSON file to given URL and displays response
-curl -sF "data=$2" "$1"
+# # Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
