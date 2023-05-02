b3365599be294c4e1d8ca31ac888976ed0ed548b
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-02 13:45:39 +0000

    Python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""
-defines function to find peak in list of unsorted integers
-"""
-
-
 def find_peak(list_of_integers):
-    """Finds a peak in a list of integers"""
-    if not list_of_integers:
-        return None
-    return fp(list_of_integers, 0, len(list_of_integers) - 1,
-              len(list_of_integers))
+    """ Python function to find peak number"""
+    le = len(list_of_integers)
+    if le == 0:
+        return
+    m = le // 2
+    pivot = list_of_integers[m]
+    left = list_of_integers[m - 1]
 
-
-def fp(arr, low, high, n):
-    """Helper func"""
-    mid = low + (high - low)//2
-
-    if (mid == 0 or arr[mid - 1] <= arr[mid]) and\
-       (mid == n - 1 or arr[mid + 1] <= arr[mid]):
-        return arr[mid]
-    elif (mid > 0 and arr[mid - 1] > arr[mid]):
-        return fp(arr, low, mid - 1, n)
-    else:
-        return fp(arr, mid + 1, high, n)
+    if (m == le - 1 or pivot >= list_of_integers[m + 1]) and\
+            (m == 0 or pivot >= left):
+        return pivot
+    elif m != le - 1 and list_of_integers[m + 1] > pivot:
+        return (find_peak(list_of_integers[m + 1:]))
+    return (find_peak(list_of_integers[:m]))
