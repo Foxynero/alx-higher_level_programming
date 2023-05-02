edc0e1b9fa8a495eda30844095a3ead8d664a245
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-02 15:02:00 +0000

    Python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,14 @@
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
+
+
 if __name__ == "__main__":
     import urllib.request as request
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         utf8_content = content.decode("UTF-8")
-        print("Body response:")
+        print('Body response:')
         print('\t- type: {}'.format(type(content)))
         print('\t- content: {}'.format(content))
         print('\t- utf8 content: {}'.format(utf8_content))
